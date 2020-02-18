9fdac4de0f91d255804953cc549c2c2a4cfafb26
bdeps <61045858+bdeps@users.noreply.github.com>
2020-02-18 20:36:37 +0100

    SigmaIT - Domain Joiner

---
+++ GUIDomainJoin.ps1
@@ -0,0 +1,410 @@
+﻿#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+# Init PowerShell Gui
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+## Set the script execution policy for this process
+Try { Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force -ErrorAction 'Stop' } Catch {}
+
+#---------------------------------------------------------[Variables]--------------------------------------------------------
+$global:domainName = ""
+$global:newComputerName = ""
+$global:ouPath = ""
+$global:domainCredentials = ""
+## Laptop/Desktop - Used for AD Groups
+$global:computerType = ""
+$regInstallation = "SigmaIT\DomainJoiner"
+
+
+
+
+
+#---------------------------------------------------------[Change Area Below]-------------------------------------------------
+## Edit this to change what domains you can join
+$availableDomains = 'contoso.com','contoso2.com'
+$domain1 = 'contoso.com'
+$domain2 = 'contoso2.com'
+
+## Edit the name of the corporation here (Example, Volvo)
+$corporateName = 'Sigma IT'
+
+## Enter the local admin domain group used
+$domainLocalAdmins = 'LINE\LIAL_G_PC_ADMIN'
+#---------------------------------------------------------[Change Area Above]-------------------------------------------------
+
+
+
+
+
+#---------------------------------------------------------[Form]--------------------------------------------------------
+
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+## Main Window
+$DomainJoinerForm = New-Object System.Windows.Forms.Form
+$DomainJoinerForm.ClientSize = '350,200'
+$DomainJoinerForm.Text = $corporateName
+$DomainJoinerForm.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+$DomainJoinerForm.TopMost = $true
+
+# main Window Description
+$domainJoinerDescription = New-Object System.Windows.Forms.Label
+$domainJoinerDescription.Text = "Please fill the fields"
+$domainJoinerDescription.AutoSize = $false
+$domainJoinerDescription.Width = 220
+$domainJoinerDescription.Height = 20
+$domainJoinerDescription.Location = New-Object System.Drawing.Point(100,30)
+$domainJoinerDescription.Font = 'Microsoft Sans Serif,8'
+
+## Domain dropdown
+$domainPicker = New-Object System.Windows.Forms.ComboBox
+$domainPicker.Text = ""
+$domainPicker.Width = 180
+$domainPicker.Height = 20
+@($availableDomains) | ForEach-Object{[void] $domainPicker.Items.Add($_)}
+$domainPicker.SelectedIndex = 0
+$domainPicker.Location = New-Object System.Drawing.Point(100,50)
+$domainPicker.Visible = $true
+$domainPicker.DropDownStyle = 'DropDownList'
+
+## Domain Dropdown Description
+$domainPickerDescription = New-Object System.Windows.Forms.Label
+$domainPickerDescription.Text = "Domain:"
+$domainPickerDescription.AutoSize = $false
+$domainPickerDescription.Width = 50
+$domainPickerDescription.Height = 20
+$domainPickerDescription.Location = New-Object System.Drawing.Point(50,54)
+
+## Computer name input
+$computerNameBox = New-Object System.Windows.Forms.TextBox
+$computerNameBox.Text = "Enter new computer name"
+$computerNameBox.Width = 180
+$computerNameBox.Height = 20
+$computerNameBox.Location = New-Object System.Drawing.Point(100,80)
+
+## Computer name input description
+$computerNameBoxDescription = New-Object System.Windows.Forms.Label
+$computerNameBoxDescription.Text = "Computer Name:"
+$computerNameBoxDescription.AutoSize = $false
+$computerNameBoxDescription.Width = 150
+$computerNameBoxDescription.Height = 20
+$computerNameBoxDescription.Location = New-Object System.Drawing.Point(10,84)
+
+## Validate Button
+$validateInputBtn = New-Object System.Windows.Forms.Button
+$validateInputBtn.Text = "Validate"
+$validateInputBtn.AutoSize = $false
+$validateInputBtn.Width = 90
+$validateInputBtn.Height = 20
+$validateInputBtn.Location = New-Object System.Drawing.Point(100,150)
+
+## Domain Join Button
+$domainJoinBtn = New-Object System.Windows.Forms.Button
+$domainJoinBtn.Text = "Join"
+$domainJoinBtn.Width = 90
+$domainJoinBtn.Height = 20
+$domainJoinBtn.Location = New-Object System.Drawing.Point(200,150)
+$domainJoinBtn.Visible = $false
+
+## Activate all set objects
+$DomainJoinerForm.controls.AddRange(@($domainJoinerDescription,$domainPicker,$domainPickerDescription,$computerNameBox,$computerNameBoxDescription,$validateInputBtn,$domainJoinBtn))
+
+## Progress Window
+$progressForm = New-Object System.Windows.Forms.Form
+$progressForm.ClientSize = '350,100'
+$progressForm.Text = "Sigma Domain Joiner"
+$progressForm.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+$progressForm.Visible = $false
+$progressForm.TopMost = $true
+
+## Progress Window Description
+$progressFormDescription = New-Object System.Windows.Forms.Label
+$progressFormDescription.Text = ""
+$progressFormDescription.Width = 350
+$progressFormDescription.Height = 100
+$progressFormDescription.AutoSize = $false
+$progressFormDescription.TextAlign = 'MiddleCenter'
+
+## Progress Window Bind
+$progressForm.controls.AddRange(@($progressFormDescription))
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+Function postJoinActionsPreparation{
+    ## Actions after the domain was Successfully Joined
+    $progressFormDescription.Text = "Creating Directories for postJoinActions"
+    Start-Sleep -Seconds 2
+
+    ## Creating folders and copying in the follow up script that will install the CM-client 
+    $postJoinScriptPath = "C:\IT"
+    
+    if(!(Test-Path $postJoinScriptPath)){
+        New-Item -Path "C:\" -Name "IT" -ItemType "Directory"
+    }
+    Start-Sleep -Seconds 3
+
+    ## Gets Path if the file is compiled to an EXE
+    $scriptPath = ".\postJoinActions.exe"
+    Copy-Item -Path $scriptPath -Destination $postJoinScriptPath -Force
+
+    $progressFormDescription.Text = "Directory and Script copy succeded"
+    Start-Sleep -Seconds 2
+
+    ## Setting up a single autologon. Password is immediately discarded and removed from registry after a restart
+    $progressFormDescription.Text = "Setting up one autologin"
+    $tempPass = $global:domainCredentials.GetNetworkCredential().Password
+    Set-AutoLogon -DefaultUsername $global:domainCredentials.UserName -DefaultPassword $tempPass -AutoLogonCount 1 -Script "c:\it\postJoinActions.exe"
+    $tempPass = ""
+    Start-Sleep -Seconds 2
+
+    $progressFormDescription.Text = "Setting up local Admin"
+    ## Adding $domainCredentials account to local admin group
+    $adminGroupName = gwmi win32_group -filter "LocalAccount = $TRUE And SID = 'S-1-5-32-544'" | select -expand name
+    
+    ## Adding LIAL_G_PC_ADMIN as a local admin group 
+    net localgroup $adminGroupName $domainLocalAdmins /add
+    Start-Sleep -Seconds 2
+    $progressFormDescription.Text = "Local Admin Setup Complete"
+    Start-Sleep -Seconds 2
+
+    $progressFormDescription.Text = "First phase complete. 
+    The computer will now restart in 10 sconds and commence the second phase"
+    Start-Sleep -Seconds 10
+    
+    ## Closing the progression Form
+    $progressForm.Close()
+    Restart-Computer
+}
+
+
+
+Function Set-AutoLogon{
+<#
+.Synopsis
+Here is the PowerShell CmdLet that would enable AutoLogon next time when the server reboots.We could trigger a specific Script to execute after the server is back online after Auto Logon.
+The CmdLet has the follwing parameter(s) and function(s).
+-DefaultUsername : Provide the username that the system would use to login.
+-DefaultPassword : Provide the Password for the DefaultUser provided.
+-AutoLogonCount : Sets the number of times the system would reboot without asking for credentials.Default is 1.
+-Script : Provide Full path of the script for execution after server reboot. Example : c:\test\run.bat
+
+Mandatory Parameters 
+-DefaultUsername 
+-DefaultPassword 
+
+
+.Description
+Here is the PowerShell CmdLet that would enable AutoLogon next time when the server reboots.We could trigger a specific Script to execute after the server is back online after Auto Logon.
+
+.Example
+Set-AutoLogon -DefaultUsername "win\admin" -DefaultPassword "password123"
+
+.Example
+Set-AutoLogon -DefaultUsername "win\admin" -DefaultPassword "password123" -AutoLogonCount "3"
+
+
+.EXAMPLE
+Set-AutoLogon -DefaultUsername "win\admin" -DefaultPassword "password123" -Script "c:\test.bat"
+
+#>
+    [CmdletBinding()]
+    Param(
+        
+        [Parameter(Mandatory=$True,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [String[]]$DefaultUsername,
+
+        [Parameter(Mandatory=$True,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [String[]]$DefaultPassword,
+
+        [Parameter(Mandatory=$False,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [AllowEmptyString()]
+        [String[]]$AutoLogonCount,
+
+        [Parameter(Mandatory=$False,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [AllowEmptyString()]
+        [String[]]$Script
+                
+    )
+
+    Begin
+    {
+        #Registry path declaration
+        $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+        $RegROPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
+    
+    }
+    
+    Process
+    {
+
+        try
+        {
+            #setting registry values
+            Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String  
+            Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String  
+            Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
+            if($AutoLogonCount)
+            {
+                
+                Set-ItemProperty $RegPath "AutoLogonCount" -Value "$AutoLogonCount" -type DWord
+            
+            }
+            else
+            {
+
+                Set-ItemProperty $RegPath "AutoLogonCount" -Value "1" -type DWord
+
+            }
+            if($Script)
+            {
+                
+                Set-ItemProperty $RegROPath "(Default)" -Value "$Script" -type String
+            
+            }
+            else
+            {
+            
+                Set-ItemProperty $RegROPath "(Default)" -Value "" -type String
+            
+            }        
+        }
+
+        catch
+        {
+
+            Write-Output "An error had occured $Error"
+            
+        }
+    }
+    
+    End
+    {
+        
+        #End
+
+    }
+
+}
+
+## Validates all form entries
+function validateInputFunction{
+
+    ## Set Variables
+    $global:domainName = $domainPicker.SelectedItem.ToString()
+    $global:newComputerName = $computerNameBox.Text
+    $global:credHelper = ""
+    $global:ouPath = ""
+    $global:computerType = ""
+    $progressFormDescription.Text = "Initializing Settings"
+    $domainJoinBtn.Visible = $false
+
+    ## Check if a computer name has been entered
+    if($global:newComputerName -eq "" -Or $global:newComputerName -eq "Enter new computer name"){
+        [System.Windows.Forms.MessageBox]::Show('Please enter a computer name')
+    }
+
+    ## Checks and creates registry keys to keep track of domain during restart
+    $registryPath = "HKLM:\Software\$regInstallation"
+    $name = "DomainJoined"
+    $value = ""
+    if(!(Test-Path $registryPath)){
+        New-Item -Path $registryPath -Force | Out-Null
+    }
+
+    ## Checks Domain and Initializes variables
+    if($global:domainName -eq $domain1){
+        ## Setting a registry value to check what domain was joined after restart
+        New-ItemProperty -Path $registryPath -Name $name -Value $domain1 -PropertyType String -Force | Out-Null
+    }
+    elseif($global:domainName -eq $domain2){
+        ## Setting a registry value to check what domain was joined after restart
+        New-ItemProperty -Path $registryPath -Name $name -Value $domain2 -PropertyType String -Force | Out-Null
+    }
+    if(!($global:computerType -eq "")){
+        ## If we were able to find a valid computer type, show domainJoinBtn
+        $domainJoinBtn.Visible = $true
+    }
+}
+
+## Removes the template text if the computerNameBox is selected
+function focusComputerNameBox{
+    if($computerNameBox.Text -eq 'Enter new computer name'){
+        $computerNameBox.Text = ''
+    }
+}
+
+## Adds the template text if computerNameBox was left empty
+function lostFocusComputerNameBox{
+    if($computerNameBox.Text -eq ''){
+        $computerNameBox.Text = 'Enter new computer name'
+    }
+}
+
+function joinDomain{
+
+    ## Hiding main window and showing Progress Window
+    $domainJoinerForm.Visible = $false
+
+    $global:domainCredentials = Get-Credential -UserName "" -Message "Please enter your ActiveDirectory Admin Account"
+    $progressForm.Visible = $true
+
+    Start-Sleep -Seconds 2
+
+    if($global:newComputerName -eq $env:COMPUTERNAME){
+        try{
+            $progressFormDescription.Text = "Attempting to join the Domain"
+            ## Crashes if you specify OU Path
+            Add-Computer -DomainName $global:domainName -Credential $global:domainCredentials -Options JoinWithNewName,accountcreate -WarningAction SilentlyContinue -ErrorAction Stop
+        }
+        catch{
+            $string_Err = $_ | Out-String
+            [System.Windows.Forms.MessageBox]::Show($string_Err)
+            Start-Sleep -Seconds 20
+            Exit
+        }
+    }
+    else{
+        try{
+            $progressFormDescription.Text = "Attempting to rename Computer"
+            Rename-Computer -NewName $global:newComputerName -Force -WarningAction SilentlyContinue
+            Start-Sleep -Seconds 8
+
+            ## There is a bug in Windows Servers 2012 R2 DC causing the renaming to fail. Uncomment this line when you have 2016 DCs
+            ## https://support.microsoft.com/sv-se/help/3152220/directory-service-is-busy-error-when-you-rename-a-domain-joined-comput
+            ## Add-Computer -NewName $global:newComputerName -DomainName $global:domainName -Credential $global:credHelper -OUPath $global:ouPath -Options JoinWithNewName,accountcreate -ErrorAction Stop
+
+            $progressFormDescription.Text = "Attempting to join the Domain"
+            Start-Sleep -Seconds 5
+            ## Crashes if you specify OU Path
+            Add-Computer -Credential $global:domainCredentials -DomainName $global:domainName -Options AccountCreate, JoinWithNewName -WarningAction SilentlyContinue -ErrorAction Stop
+            Start-Sleep -Seconds 5
+
+        }
+        catch{
+            $string_Err = $_ | Out-String
+            [System.Windows.Forms.MessageBox]::Show($string_Err)
+            Start-Sleep -Seconds 20
+            Exit
+        }
+    }
+    
+    ## Domain successfully joined
+    $progressFormDescription.Text = "Domain joined Successfully"
+    Start-Sleep -Seconds 2
+    ## Initiates preparation for restart och CM-client install
+    postJoinActionsPreparation
+}
+
+#-----------------------------------------------------------[Bindings]------------------------------------------------------------
+## Binds click event to validateInputBtn
+$validateInputBtn.Add_Click({ validateInputFunction })
+
+## Binds OnFocus event to computerNameBox
+$computerNameBox.Add_GotFocus({ focusComputerNameBox })
+
+## Binds LostFocus event to computerNameBox
+$computerNameBox.Add_LostFocus({ lostFocusComputerNameBox })
+
+## Bind the joinDomain event to joinDomainBtn
+$domainJoinBtn.Add_Click({ joinDomain })
+
+[void]$DomainJoinerForm.ShowDialog()
---
+++ postJoinActions.ps1
@@ -0,0 +1,168 @@
+﻿#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+# Init PowerShell Gui
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+## Set the script execution policy for this process
+Try { Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force -ErrorAction 'Stop' } Catch {}
+
+
+
+
+#---------------------------------------------------------[Change Area Below]-------------------------------------------------
+## Add path to the clienthealth script for SCCM-agent installation (we are domain joined at this point and will be running with your admin credentials. We can access servers on the domain)
+## Example path below
+$global:sCCMClientHealthPath = "\\servername\clienthealth\install.cmd"
+
+## AD-Groups that the computer object should be members of
+$global:computerAdGroup = "AD_GROUP_Office365"
+
+## Add the domain names (same names as in the GUIDomainJoin.ps1. It will check the registry value what domain was joined 
+## Since we dont have the AD-module it's a quick way to check
+$global:domain1 = ""
+$global:domain2 = ""
+
+## Add the OU-Path where the computer-object should be moved to for each domain
+## Example: "OU=DeployedComputers,OU=Corporate,DC=Contoso,DC=com"
+$global:domain1ComputerOuPath1 = ""
+$global:domain1ComputerOuPath2 = ""
+
+#---------------------------------------------------------[Change Area Above]-------------------------------------------------
+
+
+
+
+
+
+
+#---------------------------------------------------------[Variables]--------------------------------------------------------------
+$global:registryPath = "HKLM:\Software\SigmaIT\DomainJoiner"
+$global:name = "DomainJoined"
+$global:sccmInstallFile = Split-Path -Path $global:sCCMClientHealthPath -leaf
+
+#---------------------------------------------------------[Form]--------------------------------------------------------
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+## Progress Window
+$progressForm = New-Object System.Windows.Forms.Form
+$progressForm.ClientSize = '350,100'
+$progressForm.Text = "Sigma Domain Joiner"
+$progressForm.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+$progressForm.Visible = $false
+$progressForm.TopMost = $true
+
+## Progress Window Description
+$progressFormDescription = New-Object System.Windows.Forms.Label
+$progressFormDescription.Text = ""
+$progressFormDescription.Width = 350
+$progressFormDescription.Height = 100
+$progressFormDescription.AutoSize = $false
+$progressFormDescription.TextAlign = 'MiddleCenter'
+
+## Progress Window Bind
+$progressForm.controls.AddRange(@($progressFormDescription))
+
+[void]$progressForm.Show()
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+## ADSI function to add AD groups to users/computers
+function addToADGroup([string]$groupName,[string]$account){
+    $progressFormDescription.Text = 'Adding computer to AD Group'
+    Start-Sleep -Seconds 2
+
+    $accountDn = $null
+    $addtogroupDn = $null
+    ## computer requires $
+        if(!($accountDn = ([ADSISEARCHER]"sAMAccountName=$($account)").FindOne().Path)){$accountDn = ([ADSISEARCHER]"sAMAccountName=$($account)$").FindOne().Path}
+        $addtogroupDn = ([ADSISEARCHER]"sAMAccountName=$($groupName)").FindOne().Path
+        $group = [ADSI]"$addtogroupDn"
+            if(!($group.IsMember($accountDn))) {
+                # add to group
+                $group.Add($accountDn)
+                $progressFormDescription.Text = "$account added to $groupName"
+                Start-Sleep -Seconds 2
+                }
+                else{
+                    # already member of group
+                    $progressFormDescription.Text = 'Already a member of group'
+                    Start-Sleep -Seconds 2
+                }
+}
+
+## Installs the CCM client through the ClientHealth script. Path needs to be set in the variable change area.
+function installSCCMClient{
+    $progressFormDescription.Text = 'Installing SCCM Client. This might take up to 10 minutes'
+    Copy-Item $global:sCCMClientHealthPath 'c:\it'
+    Start-Sleep -Seconds 2
+    Start-Process -Verb RunAs -Wait cmd.exe -Args '/c', "c:\it\$global:sccmInstallFile"
+    $progressFormDescription.Text = "SCCM Client installation complete"
+    Start-Sleep -Seconds 2
+
+    ## Remove default login user
+    $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+    Set-ItemProperty $RegPath "DefaultUsername" -Value "" -type String 
+}
+
+## moveComputerOU - moves the computer the script is being run on to the target ou
+## enter a new OU with the following syntax: "OU=Users,OU=Ship Info Screen,OU=Test,OU=Offshore,OU=ISOLATION,OU=Corporate,DC=line,DC=stenanet, DC=com"
+## If you get referral errors please double check the OU path.
+function moveComputerOU([string]$newOU){
+    # Retrieve DN of local computer.
+    $SysInfo = New-Object -ComObject "ADSystemInfo"
+    $ComputerDN = $SysInfo.GetType().InvokeMember("ComputerName", "GetProperty", $Null, $SysInfo, $Null)
+
+    # Bind to computer object in AD.
+    $Computer = [ADSI]"LDAP://$ComputerDN"
+
+    # Bind to target OU.
+    $OU = [ADSI]"LDAP://$newOU"
+
+    # Move computer to target OU.
+    $Computer.psbase.MoveTo($OU)
+    $progressFormDescription.Text = "Moved Computer to DeployedComputers successfully"
+}
+#-----------------------------------------------------------[Actions]------------------------------------------------------------
+$progressFormDescription.Text = "Checking Trust/Relationship"
+
+## Wait for network connection before testing trust/relationship
+Start-Sleep -Seconds 60
+
+if((Test-ComputerSecureChannel) -eq $false){
+    $progressFormDescription.Text = "Issue with Trust found. Attempting to repair"
+    Test-ComputerSecureChannel -Repair
+}
+
+## Dekstop add
+Start-Sleep -Seconds 2
+$progressFormDescription.Text = "Adding computer to NoApplication AD-Group"
+addToADGroup -groupName $global:computerAdGroup -account $env:COMPUTERNAME
+
+## Installs the SCCM Client
+installSCCMClient
+
+Start-Sleep -Seconds 2
+## Attempting to move the computer AD object to deployed computers
+$progressFormDescription.Text = "Moving Computer to DeployedComputers"
+
+##Check what domain is joined
+if(((Get-ItemProperty -Path $global:registryPath -Name $global:name)).DomainJoined -eq $global:domain1){
+    $progressFormDescription.Text = "Moving Computer to DeployedComputers in $global:domain1"
+    moveComputerOU -newOU $global:domain1ComputerOuPath1
+    Start-Sleep -Seconds 2
+}
+elseif(((Get-ItemProperty -Path $global:registryPath -Name $global:name)).DomainJoined -eq $global:domain2){
+    $progressFormDescription.Text = "Moving Computer to DeployedComputers in $global:domain2"
+    moveComputerOU -newOU $global:domain1ComputerOuPath2
+    Start-Sleep -Seconds 2
+}
+else{
+        $progressFormDescription.Text = "Unable to find registry value at HKLM:\Software\SigmaIT\DomainJoiner
+        Client will not be moved"
+        Start-Sleep -Seconds 5
+}
+
+## Restart computer
+$progressFormDescription.Text = "Install complete. The computer will restart in 10 seconds"
+Start-Sleep -Seconds 10
+Restart-Computer
